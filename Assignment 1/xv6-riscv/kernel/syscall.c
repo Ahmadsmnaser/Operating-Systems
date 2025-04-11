69627e0978966095148196124119b0dff956290b
Ahmad Naser <157128325+Ahmadsmnaser@users.noreply.github.com>
2025-04-11 15:37:16 +0300

    Update syscall.c

--- Assignment 1/xv6-riscv/kernel/syscall.c
+++ Assignment 1/xv6-riscv/kernel/syscall.c
@@ -128,6 +128,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_memsize] sys_memsize,
+[SYS_forkn] sys_forkn,
+[SYS_waitall] sys_waitall,
 };
 
 void
